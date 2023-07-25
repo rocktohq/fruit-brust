395e86e65e3ded1566b42d4152aeb9e7d957afc3
Saidul Mursalin <itzmonir@gmail.com>
2023-07-26 00:21:08 +0600

    Initial Update

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+/*
+# TailWind Configuration
+# TailWind Custom Colors and Background Images Classes
+*/
+// Tailwind Custom Colors
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+
+            },
+            backgroundImage: {
+
+            }
+        }
+    }
+}
